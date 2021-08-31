09e578125212ef76d4c8d2a0040bb2177fbbbe65
Rb Bharti <47843771+bhartiRB@users.noreply.github.com>
2021-08-31 12:11:07 +0530

    Update Git_jenkins_intergation.java
    Testing GIt Poll SCM  in code

--- Git_jenkins_intergation.java
+++ Git_jenkins_intergation.java
@@ -1,5 +1,6 @@
 public class Git_jenkins_intergation {
   public static void main(String args[]){
   System.out.println("----******  Git_jenkins_intergation  ******------");	 
+  System.out.println("----******  Testing GIt Poll SCM  ******------");
  }
 }
